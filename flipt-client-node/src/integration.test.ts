5c420709699e7669cac5ca338ea4d45c47642b3e
Yoofi Quansah <ybquansah@gmail.com>
2024-01-05 13:58:48 -0600

    feat: Add Basic ITs for node, and python

---
+++ flipt-client-node/src/integration.test.ts
@@ -0,0 +1,96 @@
+import { FliptClient } from ".";
+
+const fliptUrl = process.env["FLIPT_URL"];
+if (!fliptUrl) {
+  console.error("please set the FLIPT_URL environment variable");
+  process.exit(1);
+}
+
+const authToken = process.env["FLIPT_AUTH_TOKEN"];
+if (!authToken) {
+  console.error("please set the FLIPT_AUTH_TOKEN environment variable");
+  process.exit(1);
+}
+
+test("variant", async () => {
+  const client = new FliptClient(fliptUrl, authToken);
+
+  const variant = await client.evaluation.variant({
+    namespace_key: "default",
+    flag_key: "flag1",
+    entity_id: "entity",
+    context: { fizz: "buzz" }
+  });
+
+  expect(variant.flagKey).toEqual("flag1");
+  expect(variant.match).toEqual(true);
+  expect(variant.reason).toEqual("MATCH_EVALUATION_REASON");
+  expect(variant.variantKey).toEqual("variant1");
+  expect(variant.segmentKeys).toContain("segment1");
+});
+
+test("boolean", async () => {
+  const client = new FliptClient(fliptUrl, authToken);
+
+  const boolean = await client.evaluation.boolean({
+    namespace_key: "default",
+    flag_key: "flag_boolean",
+    entity_id: "entity",
+    context: { fizz: "buzz" }
+  });
+
+  expect(boolean.flagKey).toEqual("flag_boolean");
+  expect(boolean.enabled).toEqual(true);
+  expect(boolean.reason).toEqual("MATCH_EVALUATION_REASON");
+});
+
+test("batch", async () => {
+  const client = new FliptClient(fliptUrl, authToken);
+  const batch = await client.evaluation.batch({
+    requests: [
+      {
+        namespace_key: "default",
+        flag_key: "flag1",
+        entity_id: "entity",
+        context: { fizz: "buzz" }
+      },
+      {
+        namespace_key: "default",
+        flag_key: "flag_boolean",
+        entity_id: "entity",
+        context: { fizz: "buzz" }
+      },
+      {
+        namespace_key: "default",
+        flag_key: "notfound",
+        entity_id: "entity",
+        context: {}
+      }
+    ]
+  });
+
+  expect(batch.responses).toHaveLength(3);
+
+  // Variant
+  expect(batch.responses[0].type).toEqual("VARIANT_EVALUATION_RESPONSE_TYPE");
+  const variant = batch.responses[0].variantResponse;
+  expect(variant.flagKey).toEqual("flag1");
+  expect(variant.match).toEqual(true);
+  expect(variant.reason).toEqual("MATCH_EVALUATION_REASON");
+  expect(variant.variantKey).toEqual("variant1");
+  expect(variant.segmentKeys).toContain("segment1");
+
+  // Boolean
+  expect(batch.responses[1].type).toEqual("BOOLEAN_EVALUATION_RESPONSE_TYPE");
+  const boolean = batch.responses[1].booleanResponse;
+  expect(boolean.flagKey).toEqual("flag_boolean");
+  expect(boolean.enabled).toEqual(true);
+  expect(boolean.reason).toEqual("MATCH_EVALUATION_REASON");
+
+  // Error
+  expect(batch.responses[2].type).toEqual("ERROR_EVALUATION_RESPONSE_TYPE");
+  const error = batch.responses[2].errorResponse;
+  expect(error.flagKey).toEqual("notfound");
+  expect(error.namespaceKey).toEqual("default");
+  expect(error.reason).toEqual("NOT_FOUND_ERROR_EVALUATION_REASON");
+});
