937f084099ba1fe0e864d39f3e83cd77458c869e
Tom Dent <tomdenty1@gmail.com>
2024-08-23 12:54:45 +0100

    fix(python): Cheered the linter up, moreso.
    Signed-off-by: Tom Dent <tomdenty1@gmail.com>

--- flipt-python/flipt/authentication/__init__.py
+++ flipt-python/flipt/authentication/__init__.py
@@ -1,7 +1,6 @@
 import datetime
 import requests
 
-from datetime import datetime
 from pathlib import Path
 
 
@@ -15,7 +14,7 @@ class ClientTokenAuthentication(AuthenticationStrategy):
         self.token = token
 
     def authenticate(self, headers: dict[str, str]) -> None:
-        headers['Authorization'] = f'Bearer {self.token}'
+        headers['Authorization'] = f"Bearer {self.token}"
 
 
 class JWTAuthentication(AuthenticationStrategy):
@@ -23,14 +22,14 @@ class JWTAuthentication(AuthenticationStrategy):
         self.token = token
 
     def authenticate(self, headers: dict[str, str]) -> None:
-        headers['Authorization'] = f'JWT {self.token}'
+        headers['Authorization'] = f"JWT {self.token}"
 
 
 class KubernetesAuthentication(AuthenticationStrategy):
+    default_service_token_path = '/var/run/secrets/kubernetes.io/serviceaccount/token' # pylint: disable=S107
     def __init__(self,
                  token: str,
-                 # ruff: disable=S107
-                 service_account_token_path:str = '/var/run/secrets/kubernetes.io/serviceaccount/token') -> None:
+                 service_account_token_path:str = default_service_token_path) -> None:
         self.token = token
         self.service_account_token_path = service_account_token_path
         self.token_expiry = None
@@ -38,7 +37,7 @@ class KubernetesAuthentication(AuthenticationStrategy):
     def authenticate(self, headers: dict[str, str]) -> None:
         # Check if token is available and not expired.
         if self.token and self.token_expiry and self.token_expiry > datetime.now(tz=datetime.timezone.utc).timestamp():
-            headers['Authorization'] = f'Bearer {self.token}'
+            headers['Authorization'] = f"Bearer {self.token}"
             return
 
         # Read service account info from the local file system.
@@ -51,9 +50,11 @@ class KubernetesAuthentication(AuthenticationStrategy):
         # Send the token to flipt's auth endpoint and collect the response.
         payload = {'service_account_token': service_account_token}
         try:
-            response = requests.post('http://flipt:8080/auth/v1/method/kubernetes/serviceaccount', json=payload, timeout=5)
+            response = requests.post('http://flipt:8080/auth/v1/method/kubernetes/serviceaccount',
+                                     json=payload,
+                                     timeout=5)
         except requests.exceptions.RequestException as e:
-            raise RuntimeError(f'Failed to authenticate with Flipt.') from e
+            raise RuntimeError('Failed to authenticate with Flipt.') from e
 
         # Parse the response, storing the token and expiration as a unix timestamp.
         try:
@@ -61,6 +62,6 @@ class KubernetesAuthentication(AuthenticationStrategy):
             self.token = response_data.get('clientToken')
             self.token_expiry = response_data.get('expiresAt')
         except (KeyError, ValueError) as e:
-            raise RuntimeError(f'Failed parsing authentication response.') from e
+            raise RuntimeError('Failed parsing authentication response.') from e
 
-        headers['Authorization'] = f'Bearer {self.token}'
+        headers['Authorization'] = f"Bearer {self.token}"
